7c9a9485d56b42b2c02e938ee24af2d16ab3ce8f
lzybluee <lzybluee@gmail.com>
2018-03-26 18:23:49 +0800

    1/2 cmc from un-set won't crash the app sync reprint.sameIndex code

--- app/src/main/java/lu/cifer/mtgviewer/CardAnalyzer.java
+++ app/src/main/java/lu/cifer/mtgviewer/CardAnalyzer.java
@@ -24,9 +24,6 @@ public class CardAnalyzer {
     public static String[] results;
     public static int exclude;
 
-    static String last_name = "";
-    static ReprintInfo last_card;
-    static int same_count = 0;
     static Hashtable<String, CardInfo> cardDatabase = new Hashtable<>();
     static int reprintCards = 0;
     static String[] allName;
@@ -46,7 +43,7 @@ public class CardAnalyzer {
     static Vector<ReprintInfo> resultCards;
     static String lastCode;
     static boolean single;
-
+    static HashMap<String, Vector<ReprintInfo>> setCards;
     static Comparator<ReprintInfo> editionComparator = new Comparator<ReprintInfo>() {
         @Override
         public int compare(ReprintInfo left, ReprintInfo right) {
@@ -59,7 +56,6 @@ public class CardAnalyzer {
             return reverse ? -ret : ret;
         }
     };
-
     static Comparator<ReprintInfo> nameComparator = new Comparator<ReprintInfo>() {
         @Override
         public int compare(ReprintInfo left, ReprintInfo right) {
@@ -76,7 +72,6 @@ public class CardAnalyzer {
             return reverse ? -ret : ret;
         }
     };
-
     static Comparator<ReprintInfo> cmcComparator = new Comparator<ReprintInfo>() {
         @Override
         public int compare(ReprintInfo left, ReprintInfo right) {
@@ -93,7 +88,6 @@ public class CardAnalyzer {
             return reverse ? -ret : ret;
         }
     };
-
     static Comparator<ReprintInfo> ratingComparator = new Comparator<ReprintInfo>() {
         @Override
         public int compare(ReprintInfo left, ReprintInfo right) {
@@ -114,7 +108,6 @@ public class CardAnalyzer {
             }
         }
     };
-
     static Comparator<ReprintInfo> colorComparator = new Comparator<ReprintInfo>() {
         @Override
         public int compare(ReprintInfo left, ReprintInfo right) {
@@ -137,6 +130,10 @@ public class CardAnalyzer {
             return reverse ? -ret : ret;
         }
     };
+
+    public static CardInfo get(String name) {
+        return cardDatabase.get(name);
+    }
 
     static boolean hasColor(CardInfo card, String longColor, String shortColor) {
         return (card.colorIndicator != null && card.colorIndicator.contains(longColor)) ||
@@ -561,7 +558,7 @@ public class CardAnalyzer {
                 if (matcher.find()) {
                     card.mana = matcher.group(1);
                     if (matcher.group(3) != null) {
-                        card.converted = Integer.parseInt(matcher.group(3));
+                        card.converted = (int) Float.parseFloat(matcher.group(3));
                     }
                 }
             }
@@ -771,34 +768,34 @@ public class CardAnalyzer {
         return reprint;
     }
 
-    public static void processCard(String str) {
+    public static ReprintInfo processCard(String str) {
+        ReprintInfo reprint = null;
         String name = getEntry(str, "Name");
 
-        if (name.equals(last_name) && same_count == 0) {
-            last_card.sameIndex = 1;
-        }
-
         if (cardDatabase.containsKey(name)) {
-            last_card = addReprintCard(str, cardDatabase.get(name));
+            reprint = addReprintCard(str, cardDatabase.get(name));
         } else {
             CardInfo card = getNewCard(getEntry(str, "Card"));
             cardDatabase.put(name, card);
             card.reprints = new Vector<>();
-            last_card = addReprintCard(str, card);
+            reprint = addReprintCard(str, card);
         }
 
-        if (name.equals(last_name)) {
-            same_count++;
-            last_card.sameIndex = same_count + 1;
+        if (setCards.containsKey(name)) {
+            setCards.get(name).add(reprint);
         } else {
-            same_count = 0;
+            Vector<ReprintInfo> vector = new Vector<>();
+            vector.add(reprint);
+            setCards.put(name, vector);
         }
-        last_name = name;
+
+        return reprint;
     }
 
     public static void processSet(File file) {
         System.out.println("Process Oracle: " + file);
         BufferedReader reader;
+        setCards = new HashMap<>();
         try {
             reader = new BufferedReader(new FileReader(file));
             String str;
@@ -814,6 +811,16 @@ public class CardAnalyzer {
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        for (String s : setCards.keySet()) {
+            if (setCards.get(s).size() > 1) {
+                int index = 1;
+                for (ReprintInfo reprint : setCards.get(s)) {
+                    reprint.sameIndex = index;
+                    index++;
+                }
+            }
         }
     }
 
